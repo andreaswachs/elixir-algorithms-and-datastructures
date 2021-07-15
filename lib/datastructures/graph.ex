dcecc148556c3919d0cfcf70347039834f686293
Andreas Wachs Hjalager <cwrp83@motorolasolutions.com>
2021-07-15 13:19:59 +0200

    Finalize add edge to graph

--- lib/datastructures/graph.ex
+++ lib/datastructures/graph.ex
@@ -8,13 +8,19 @@ defmodule Graph do
 
   The graph will be implemented as an adjacency list, with the vertices as keys in a map
   This means that the keys in the graph map will be the vertex number that will hold a value
-  that is a list of Edge structs that hold properties of each given edge
-  """
-  defstruct vertices: 0, edges: 0
+  that is a list of Edge structs that hold properties of each given edge.
 
-  @spec new(Integer) :: {:error, String.t()} | {:ok, %Graph{edges: 0, vertices: 0}}
-  def new(size) when size > 0 do
-    create_graph(%Graph{}, 0, size)
+  The graph defaults to being an undirected graph, if you're not inserting
+  """
+  defstruct vertices: 0, edges: 0, directed: false
+
+  @spec new(non_neg_integer(), boolean()) :: %Graph{
+          :directed => any,
+          :edges => 0,
+          :vertices => non_neg_integer
+        }
+  def new(size, directed \\ false) when size > 0 do
+    create_graph(%Graph{directed: directed}, 0, size)
   end
 
   defp create_graph(graph, vertex, limit) when vertex >= limit do
@@ -44,13 +50,14 @@ defmodule Graph do
           :vertices => any,
           optional(any) => any
         }
-  def add_edge(%Graph{vertices: vertices, edges: edges} = graph, from, to, weight \\ 0, directed \\ false)
+  def add_edge(%Graph{vertices: vertices, edges: edges} = graph, from, to, weight \\ 0, insert_inverse_edge \\ false)
       when from < vertices and to < vertices do
         graph
         |> Map.get(from, [])
         |> List.insert_at(0, Edge.new(from, to, weight))
         |> then(fn adj_list -> %{graph | from => adj_list, edges: edges + 1} end)
-        |> then(fn new_graph -> if directed, do: add_edge(new_graph, to, from, weight), else: new_graph end)
+        # figure out why this goes into an infinite loop
+        |> then(fn new_graph -> if not graph.directed and not insert_inverse_edge, do: add_edge(new_graph, to, from, weight, true), else: new_graph end)
   end
 
 
