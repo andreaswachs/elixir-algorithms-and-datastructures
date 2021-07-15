a1172494221b48fefb58b247a3f1ea8fba2bf9d4
Andreas Wachs Hjalager <cwrp83@motorolasolutions.com>
2021-07-15 11:44:00 +0200

    Add new function to edge module

--- lib/datastructures/edge.ex
+++ lib/datastructures/edge.ex
@@ -1,9 +1,9 @@
 defmodule Edge do
 
-  defstruct from: nil, to: nil, weight: 0, directed: false
+  defstruct from: nil, to: nil, weight: 0
 
-  @spec new(non_neg_integer(), non_neg_integer(), integer(), boolean()) :: %Edge{}
-  def new(to, from, weight \\ 0, directed \\ false) do
-    %Edge{to: to, from: from, weight: weight, directed: directed}
+  @spec new(non_neg_integer(), non_neg_integer(), integer()) :: %Edge{}
+  def new(from, to, weight \\ 0) do
+    %Edge{to: to, from: from, weight: weight}
   end
 end
--- lib/datastructures/graph.ex
+++ lib/datastructures/graph.ex
@@ -4,16 +4,17 @@ defmodule Graph do
   Common operations will also be supported in the future,
   but algorithms that utilize a graph data structure might be
   written elsewhere (not sure yet).
+
+
+  The graph will be implemented as an adjacency list, with the vertices as keys in a map
+  This means that the keys in the graph map will be the vertex number that will hold a value
+  that is a list of Edge structs that hold properties of each given edge
   """
-  defstruct vertices: 0, edges: 0, graph: {}
+  defstruct vertices: 0, edges: 0
 
-  @spec new(Integer) :: {:error, String.t()} | {:ok, %Graph{edges: 0, graph: {}, vertices: 0}}
+  @spec new(Integer) :: {:error, String.t()} | {:ok, %Graph{edges: 0, vertices: 0}}
   def new(size) when size > 0 do
-    {:ok, create_graph(%Graph{}, 0, size)}
-  end
-
-  def new(size) when size <= 0 do
-    {:error, "Size needs to be larger than 0"}
+    create_graph(%Graph{}, 0, size)
   end
 
   defp create_graph(graph, vertex, limit) when vertex >= limit do
@@ -30,4 +31,13 @@ defmodule Graph do
   # Functions to add edges between vertices
   # Should also support directed edges
   ####################################################################################################
+
+  def add_edge(%Graph{vertices: vertices, edges: edges} = graph, from, to, weight \\ 0, directed \\ false)
+      when from < vertices and to < vertices do
+        graph
+        |> Map.get(from, [])
+        |> List.insert_at(0, Edge.new(from, to, weight))
+        |> then(fn adj_list -> %{graph | from => adj_list, edges: edges + 1} end)
+        |> then(fn new_graph -> if directed, do: add_edge(new_graph, to, from, weight), else: new_graph end)
+  end
 end
