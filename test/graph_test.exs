9fb9f9263e2cca090482cd60df3273a898297e4e
Andreas Wachs Hjalager <cwrp83@motorolasolutions.com>
2021-07-15 13:28:22 +0200

    Add test for adding many edges to graph

--- test/graph_test.exs
+++ test/graph_test.exs
@@ -13,7 +13,7 @@ defmodule GraphTest do
     # the graph is undirected by default
     graph = Graph.new(2) |> Graph.add_edge(0, 1)
 
-    assert graph.edges == 2
+    assert graph.edges == 1
     assert graph.vertices == 2
     assert length(Map.get(graph, 0)) == 1
     assert length(Map.get(graph, 1)) == 1
@@ -26,5 +26,19 @@ defmodule GraphTest do
     assert graph.vertices == 2
     assert length(Map.get(graph, 0)) == 1
     assert length(Map.get(graph, 1)) == 0
+  end
+
+  test "can add three edges to undirected graph" do
+    graph =
+      Graph.new(3, false)
+      |> Graph.add_edge(0, 1)
+      |> Graph.add_edge(1, 2)
+      |> Graph.add_edge(2, 0)
+
+    assert graph.edges == 3
+    assert graph.vertices == 3
+    assert length(Map.get(graph, 0)) == 2
+    assert length(Map.get(graph, 1)) == 2
+    assert length(Map.get(graph, 2)) == 2
   end
 end
